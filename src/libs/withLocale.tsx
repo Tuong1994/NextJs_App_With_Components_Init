f979394955246b526efb0159bb79c68cb2ad11f1
Tuong1994 <nhambontuong68@gmail.com>
2026-05-25 12:04:16 +0700

    fix withLocale HOC

--- src/libs/withLocale.tsx
+++ src/libs/withLocale.tsx
@@ -2,13 +2,18 @@ import { use } from "react";
 import { setRequestLocale } from "next-intl/server";
 import type { ComponentType } from "react";
 
-export default function withLocale<P>(
-  Component: ComponentType<P & { locale: string }>
-) {
-  return function Wrapper({ params, ...rest }: { params: Promise<{ locale: string }> } & P) {
-    const { locale } = use(params);
-    setRequestLocale(locale);
+export type ParamProps = { id?: string; locale: string };
 
-    return <Component {...(rest as P)} locale={locale} />;
+export default function withLocale<P>(Component: ComponentType<P & { locale: string }>) {
+  return function Wrapper({ params, ...rest }: { params: Promise<ParamProps> } & P) {
+    const { locale, id } = use(params);
+
+    let paramsProps: ParamProps = { locale };
+
+    if (typeof window === "undefined") setRequestLocale(locale);
+
+    if (id) paramsProps = { ...paramsProps, id };
+
+    return <Component {...(rest as P)} {...paramsProps} />;
   };
 }
